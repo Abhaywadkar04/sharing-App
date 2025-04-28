a225e463234ac091d5b770b82deba3e29d931abe
Abhaywadkar04 <abhaywadkar2004@gmail.com>
2025-04-28 15:05:00 +0530

    done with controller error

--- server/controller/image-controller.js
+++ server/controller/image-controller.js
@@ -10,47 +10,8 @@ export const uploadImage = async (req, res) => {
     const files = await file.create(fileObj);
     console.log(files);
     // res.status(201).json({path:"http://localhost:3000/"+"file/"+files._id});
-<<<<<<< HEAD
     res.status(201).json({path:"https://sharing-app-r5f5.onrender.com"+"file/"+files._id});
 
-=======
-    import file from "../models/file.js";
-
-export const uploadImage = async (req, res) => {
-  const fileObj = {
-    path: req.file.path,
-    name: req.file.originalname,
-  };
-
-  try {
-    const files = await file.create(fileObj);
-    console.log(files);
-    // res.status(201).json({path:"http://localhost:3000/"+"file/"+files._id});
-    res.status(201).json({path:"https://sharing-app-r5f5.onrender.com"+"file/"+files._id});
-
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: "Failed to upload file" });
-  }
-};
-
-
-export const downloadImage = async (req, res) => {
-
-
-    try{
-        const files=   await file.findById(req.params.id);
-        files.downloadContent++;
-        await files.save();
-        res.download(files.path,files.name);
-    }catch(error)
-    {
-    console.log(error);
-    return res.status(500).json({ message: "Failed to download file" });
-    }
-}
-
->>>>>>> 818e4ac9964910bc436ef05edb0d3443a5939753
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Failed to upload file" });
